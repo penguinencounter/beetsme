21adf2e7d9095de16f554334192898db4ff79a8b
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-08-07 17:58:24 -0700

    fix gitignore, generate functions

--- custom.py
+++ custom.py
@@ -2,18 +2,18 @@ import shutil
 import subprocess
 import json
 import os
-from beet import Context
-from pprint import pprint
+from beet import Context, Function
 from requests import get
 import time
 
-
-MCGEN = "python -m mcgen --version release --log FATAL --processors mcgen.processors.split_registries"
+VERSION_SPECIFIC = True
+VERSION_TARGET = '1.19'
+MCGEN = f"python -m mcgen --version {VERSION_TARGET} --log FATAL --processors mcgen.processors.split_registries"
 PISTON_VERSION_MANIFEST = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
 
 
 def generate(_):
-    print('>> Building data')
+    print('\n>> Building data')
     print('> Checking ratelimit data', flush=True)
     cache_file_loc = os.path.join('temp', 'caching.json')
     if os.path.exists(cache_file_loc):
@@ -29,9 +29,10 @@ def generate(_):
     r = get(PISTON_VERSION_MANIFEST)
     # extract JSON
     data = r.json()
+    version = VERSION_TARGET if VERSION_SPECIFIC else data['latest']['release']
     print('> Checking version cache data', flush=True)
     if 'version' in extracted.keys():
-        if extracted['version'] == data['latest']['release']:
+        if extracted['version'] == version:
             print('> Data is up to date', flush=True)
             print('>> Finished', flush=True)
             return
@@ -54,7 +55,7 @@ def generate(_):
     print('>> Exporting data to discoverable folder', flush=True)
 
     # copy temp/out/(version)/reports/registries to temp/registries
-    out_registries = os.path.join('temp', 'out', data['latest']['release'], 'reports', 'registries')
+    out_registries = os.path.join('temp', 'out', version, 'reports', 'registries')
     if os.path.exists(out_registries):
         shutil.copytree(out_registries, os.path.join('temp', 'registries'))
     else:
@@ -72,13 +73,17 @@ def generate(_):
 
     print('> Writing cache', flush=True)
     with open(cache_file_loc, 'w') as f:
-        json.dump({'version': data['latest']['release'], 'time': time.time()}, f)
+        json.dump({'version': version, 'time': time.time()}, f)
     print('>> Finished generating regdump', flush=True)
 
 
-def gen_use(ctx: Context):
+def gen_place_block(ctx: Context):
+    """
+    Generates block placement hooks.
+    """
+    sbns = 'terr.'
     ns = ctx.data['territories_generated']
-    print('>> Generating use data')
+    print('\n>> Generating use data')
     print('> Accessing generator data', flush=True)
     all_blocks = os.path.join('temp', 'registries', 'block', 'data.json')
     all_items = os.path.join('temp', 'registries', 'item', 'data.json')
@@ -88,9 +93,29 @@ def gen_use(ctx: Context):
         items = json.load(f)['values']
     print('> Generating use data', flush=True)
     # Generate a list of all ids that are in both blocks and items
-    shared = []
+    shared = {}
+    fmt = 'minecraft.used:{}'
+    i = 0
     for block in blocks:
         if block in items:
-            shared.append(block)
-    pprint(shared)
+            name = f'place.{i}'
+            shared[fmt.format(block.replace(':', '.'))] = name
+            i += 1
+    initOutput = ''
+    checkOutput = ''
+    uninstallOutput = ''
+    for sbn, name in shared.items():
+        initOutput += f'scoreboard objectives add {sbns}{name} {sbn}\n'
+        checkOutput += 'execute as @a[scores={'+sbns+name+'=1..}] run function #territories:on_place\n'
+        checkOutput += 'execute as @a[scores={'+sbns+name+'=1..}] run scoreboard players set @s '+sbns+name+' 0\n'
+        uninstallOutput += f'scoreboard objectives remove {sbns}{name}\n'
+    print('== STATISTICS ==')
+    print(f'Total blocks registered: {len(shared)}')
+    print(f'Init  : {len(initOutput)} char, {len(initOutput.splitlines())} lines')
+    print(f'Hook  : {len(checkOutput)} char, {len(checkOutput.splitlines())} lines')
+    print(f'Uninst: {len(uninstallOutput)} char, {len(uninstallOutput.splitlines())} lines')
+    print('================')
 
+    # add to datapack
+    ns["create_place_scoreboards"] = Function(initOutput)
+    ns["check_place_scoreboards"] = Function(initOutput)
